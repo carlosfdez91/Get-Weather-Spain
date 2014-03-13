590ab5674be33f95ff28809eaf433cd14a4f77a5
carlosfdez91 <carlosfernandez16@gmail.com>
2014-03-13 19:15:54 +0100

    Aplicacion lista a falta de unos pequeños ajustes

--- prueba.py
+++ prueba.py
@@ -1,18 +1,41 @@
 #coding: utf-8
+import os
 import requests
 from lxml import etree
 
 yweatherns = "{http://xml.weather.yahoo.com/ns/rss/1.0}"
 
-#localidad = raw_input('¿De que ciudad quieres el tiempo? ')
+def orientacion(direccion):
+	"""Función que calcula la dirección de la que procede el viento"""
+	for grados in str(direccion):
+		if direccion >= 337 and direccion < 22.5:
+			return 'N'
+		elif direccion >= 22.5 and direccion < 67.5:
+			return 'NE'
+		elif direccion >= 67.5 and direccion < 112.5:
+			return 'E'
+		elif direccion >= 112.5 and direccion < 157.5:
+			return 'SE'
+		elif direccion >= 157.5 and direccion < 202.5:
+			return 'S'
+		elif direccion >= 202.5 and direccion < 247.5:
+			return 'SO'
+		elif direccion >= 247.5 and direccion < 292.5:
+			return 'O'
+		elif direccion >= 292.5 and direccion < 337.5:
+			return 'NO'
 
-#url = requests.get('http://xoap.weather.com/weather/search/search?'
-#	,params = {'where':'%s' % localidad})
+localidad = raw_input('¿De que ciudad quieres el tiempo? ')
 
-#raiz = etree.fromstring(url.text.encode("utf-8"))
-#loc = raiz.find("loc")
-#ciudad = loc.attrib["id"]
-ciudad = ('SPXX0081')
+espacio = localidad.replace(" ","-")
+
+url = requests.get('http://xoap.weather.com/weather/search/search?'
+	,params = {'where':'%s' % espacio})
+
+raiz = etree.fromstring(url.text.encode("utf-8"))
+loc = raiz.find("loc")
+ciudad = loc.attrib["id"]
+#ciudad = ('SPXX0081')
 
 valores = {'p': '%s' % ciudad,'u':'c'}
 tiempo = requests.get('http://weather.yahooapis.com/forecastrss?',
@@ -30,13 +53,28 @@ direccion = raiz2.find('channel/%swind' % yweatherns).attrib["direction"]
 velocidad = raiz2.find('channel/%swind' % yweatherns).attrib["speed"]
 km = raiz2.find('channel/%sunits' % yweatherns).attrib["distance"]
 speed = raiz2.find('channel/%sunits' % yweatherns).attrib["speed"]
+press = raiz2.find('channel/%sunits' % yweatherns).attrib["pressure"]
 humedad = raiz2.find('channel/%satmosphere' % yweatherns).attrib["humidity"]
+vision = raiz2.find('channel/%satmosphere' % yweatherns).attrib["visibility"]
+presion = raiz2.find('channel/%satmosphere' % yweatherns).attrib["pressure"]
+sol = raiz2.find('channel/%sastronomy' % yweatherns).attrib["sunrise"]
+puesta = raiz2.find('channel/%sastronomy' % yweatherns).attrib["sunset"]
+cielo = raiz2.find('channel/item/%scondition' % yweatherns).attrib["text"]
+reemplazar = cielo.replace("Partly Cloudy"
+	,"Parcialmente nublado").replace("Cloudy","Nublado").replace("Sunny",
+	"Soleado").replace("Fair","Noche").replace("Mostly Cloudy",
+	"Mayormente Nublado").replace("Showers","Lluvia").replace("Mostly Clear",
+	"Mayormente despejado")
 
+direcc = orientacion(direccion)
 
-print "Lugar de la consulta: %s " % tiempode
-print "Fecha y hora de la consulta: %s" % fechayhora
-print "Temperatura actual: %s %s" % (tempactual,grados)
-print "Viento: Sensacion %s %s Direccion %s %s Velocidad %s %s" % (sensacion,
-	grados,direccion,km,velocidad,speed)
-print "Caracteristicas atmosfericas: Humedad %s " % (humedad)
+os.system ('clear')
+print "-Lugar de la consulta: %s " % tiempode
+print "-Fecha y hora de la consulta: %s" % fechayhora
+print "-Temperatura actual: %s %s, Cielo: %s" % (tempactual,grados,reemplazar)
+print "-Viento: Sensacion termica %s %s, Direccion %s, Velocidad %s %s" % (
+	sensacion,grados,direcc,velocidad,speed)
+print "-Atmosfera: Humedad %s por ciento, Visibilidad %s %s, Presion %s %s" % (
+	humedad,vision,km,presion,press)
+print "-Astronomia: Amanecer: %s, Ocaso: %s" % (sol,puesta)
 
